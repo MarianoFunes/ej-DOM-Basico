e2bbb18aa65d138803c9af26d10530fd62b8d44a
MarianoFunes <mgfg83@gmail.com>
2023-10-31 21:19:38 -0300

    firt commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const h1 = document.querySelector("h1");
+const form = document.querySelector("#form"); 
+const numbA = document.querySelector("#number1"); 
+const numbB = document.querySelector("#number2"); 
+const btn = document.querySelector("#btn"); 
+
+function sumarNumbs (numbA, numbB) {
+    console.log(Number(numbA.value) + Number(numbB.value))
+}
+
